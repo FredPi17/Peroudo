ef607ee1b7314628390fcc2d9a9f479c1eea6e51
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-11-15 08:33:48 +0100

    Init Statistiques et Sauvegarde

---
+++ Perudo/Perudo/Perudo/Sauvegarde.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perudo
+{
+    class Sauvegarde
+    {
+        public void import()
+        {
+            
+        }
+        public void export()
+        {
+
+        }
+    }
+}
---
+++ Perudo/Perudo/Perudo/Statistiques.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perudo
+{
+    class Statistiques
+    {
+        public nbPartie {
+            get;
+            set;
+        }
+    }
+}
