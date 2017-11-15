8a4f4cda1b13ea45894542f67c142c869d391822
Elsa <elsa.demaine@epsi.fr>
2017-11-15 11:11:27 +0100

    Upate IA

--- Perudo/Perudo/Perudo/Backend/IA.cs
+++ Perudo/Perudo/Perudo/Backend/IA.cs
@@ -4,10 +4,25 @@ using System.Text;
 
 namespace Perudo.Backend
 {
+    enum Niveau
+    {
+        Facile,
+        Difficile
+    }
     class IA
     {
         ///Propriétés
+        private Niveau myNiveau;
         ///Constructeur
+        public IA()
+        {
+            myNiveau = Niveau.Facile;
+        }
+        public IA(Niveau niveau)
+        {
+            
+            myNiveau = Niveau.niveau;
+        }
         ///Méthodes
     }
 }
