29e4d64f1576c513b920d548ce3c08cd23018bb7
Guillaume Sayen <guillaume.sayen@epsi.fr>
2017-11-15 10:00:51 +0100

    Ajout de la classe Joueur

---
+++ Perudo/Perudo/Perudo/Joueur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perudo
+{
+    class Joueur
+    {
+        string nom;
+        string prenom;
+        string pseudo;
+    }
+}
