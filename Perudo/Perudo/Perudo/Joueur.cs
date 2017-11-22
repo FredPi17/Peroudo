e3d5d983a603923803e760e9e90272ed920181d6
Guillaume Sayen <guillaume.sayen@epsi.fr>
2017-11-22 11:07:52 +0100

    Changement CompteDés

--- Perudo/Perudo/Perudo/Joueur.cs
+++ Perudo/Perudo/Perudo/Joueur.cs
@@ -24,31 +24,6 @@ namespace Perudo
             }
             return Type;
         }
-
-        public void CompteurDes()
-        {
-            int compteur = 5; // A remplacer par la variable qui indique le nombre de dés d'un joueur
-            bool calza = false;
-            bool dudo = false;
-            bool perdu = false;
-            
-            if (compteur > 0)
-            {
-                if (dudo == true) // Si le joueur a perdu la manche
-                {
-                    compteur = compteur - 1;
-                    dudo = false;
-                }
-                if (calza == true && compteur < 5) // Si le joueur a remporté le calza
-                {
-                    compteur = compteur + 1;
-                    calza = false;
-                }
-            }
-            if (compteur == 0) // Si le joueur a perdu tous ses dés
-                perdu = true;
-
-            return (compteur, perdu);
-        }
+        int CompteDés = 5;
     }
 }
