6de81fcf37ee889068a28afb3e5a800706cfd5f5
Elsa <elsa.demaine@epsi.fr>
2017-11-27 15:06:25 +0100

    Modification de la class abstrait Joueur. Ajout des énumération  (typeJoueur, Action)

---
+++ Perudo/Perudo/Perudo/Backend/EnumType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perudo.Backend
+{
+    enum TypeJoueur
+    {
+        ordinateur = 0,
+        humain = 1
+    }
+}
--- Perudo/Perudo/Perudo/Backend/Joueur.cs
+++ Perudo/Perudo/Perudo/Backend/Joueur.cs
@@ -1,41 +1,45 @@
-﻿using System;
+﻿using Perudo.Backend;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Perudo
-{
-    class Joueur 
+{  
+    abstract class Joueur 
     {
-        enum Type
+        ///Propriétées
+        protected TypeJoueur typeJ;
+        protected int nbDes = 5;
+        protected List<Des> mesDes = new List<Des>();
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Le nombre de dés du joueur</returns>
+        int GetNbDes()
         {
-            ordinateur,
-            humain
+            return nbDes;
         }
-        public void TypeJoueur()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Une liste avec la valeur de chaque dés du Joueur</returns>
+        List<Des> GetDes()
         {
-            bool joueur = true; // A remplacer par la variable qui indique si on veut une ia ou un joueur
-            if (joueur == true)
+            return mesDes;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nbDes">Lance tous les dés du joueur</param>
+        void SetDes(int nbDes)
+        {
+            foreach(Des d in mesDes)
             {
-                Type type = Type.humain;
+                d.DiceRoll();
+                mesDes.Add(d);
             }
-            else
-            { 
-                Type type = Type.ordinateur;
-            }
-            return Type;
         }
-        int CompteDés = 5;
     }
 
-    interface Joueur
-    {
-        Type typeJoueur; //le type du joueur
-
-        int nbdes; //le nombre de des du joueur
-
-        void Joueur(Type joueur) { } //constructeur
-
-        int GetNbDes() { } //renvoi le nombre de des du joueur
-
-    }
 }
