77b8e38c20803b5a0958ae6631038d8c9b97cfb6
Elsa <elsa.demaine@epsi.fr>
2017-11-27 15:19:52 +0100

    Modification humain

--- Perudo/Perudo/Perudo/Backend/Humain.cs
+++ Perudo/Perudo/Perudo/Backend/Humain.cs
@@ -1,19 +1,32 @@
-﻿using System;
+﻿using Perudo.Backend;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Perudo.Backend
 {
-    class Humain
+    class Humain : Joueur
     {
         string nom;
         string prenom;
         string pseudo;
+
+        Humain(string pseudo)
+        {
+            typeJ = TypeJoueur.Humain;
+            this.pseudo = pseudo;
+        }
+
+        Humain(string nom, string prenom, string pseudo)
+        {
+            typeJ = TypeJoueur.Humain;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.pseudo = pseudo;
+        }
         string Getpseudo()
         {
-            string pjoueur = pseudo;
-            
-            return pjoueur;
+            return pseudo;
         }
     }
 }
