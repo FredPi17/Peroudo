abd32f9bd5926c57ed5039ac86e16bb9f9c1c5b6
Elsa <elsa.demaine@epsi.fr>
2017-11-29 09:54:28 +0100

    Modifi Humain

--- Perudo/Perudo/Perudo/Backend/Humain.cs
+++ Perudo/Perudo/Perudo/Backend/Humain.cs
@@ -7,8 +7,6 @@ namespace Perudo.Backend
 {
     public class Humain : Joueur
     {
-        private string nom;
-        private string prenom;
         private string pseudo;
 
         public Humain(string pseudo, Randomizer randomizer)
@@ -17,15 +15,7 @@ namespace Perudo.Backend
             typeJ = TypeJoueur.humain;
             this.pseudo = pseudo;
         }
-
-        public Humain(string nom, string prenom, string pseudo, Randomizer randomizer)
-            : base(randomizer)
-        {
-            typeJ = TypeJoueur.humain;
-            this.nom = nom;
-            this.prenom = prenom;
-            this.pseudo = pseudo;
-        }
+                
         public string Getpseudo()
         {
             return pseudo;
