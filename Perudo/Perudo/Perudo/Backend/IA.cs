b6f9598b2896f51b04f38e258dabdf04144155ff
Hugo Lausenaz-Pire <hugo.lausenazpire@epsi.fr>
2017-12-06 09:48:09 +0100

    Add IA File

--- Perudo/Perudo/Perudo/Backend/IA.cs
+++ Perudo/Perudo/Perudo/Backend/IA.cs
@@ -1,10 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ConsoleApp1;
 
 namespace Perudo.Backend
 {
-    class IA
+    class IA : Joueur
     {
+        ///Propriétés
+        private Niveau myNiveau;
+
+        /// <summary>
+        /// Constructeur d'IA
+        /// </summary>
+        /// <param name="choix">Le choix du niveau</param>
+        /// <param name="id">l'index ou le numéro du joueur</param>
+        /// <param name="randomizer">pour les dés</param>
+        public IA(Niveau choix, int id, Randomizer randomizer)
+            : base(id, randomizer)
+        {
+            myNiveau = choix;
+            typeJ = TypeJoueur.ordinateur;
+        }
+
+        /// <summary>
+        /// Cette méthode permet a l'IA de jouer
+        /// </summary>
+        /// <param name="de">le numéro du de</param>
+        /// <param name="nb">le nombre de des</param>
+        /// <param name="nbDeTable">le nombre de des encore en jeu</param>
+        public void Jouer(int de, int nb, int nbDeTable)
+        {
+            switch (myNiveau)
+            {
+                case Niveau.Facile:
+                    {
+                        Random rng = new Random();
+                        int choix = rng.Next(0, 6);
+                        if (choix == 0)
+                        {
+                            ///bluff
+                        }
+                        else if (choix == 1)
+                        {
+                            ///encherir sur de
+                        }
+                        else if (choix == 2)
+                        {
+                            ///encherir sur nb
+                        }
+                        else if (choix == 3)
+                        {
+                            ///encherir perudo
+                        }
+                        else if (choix == 4)
+                        {
+                            ///encherir de et nb
+                        }
+                        else if (choix == 5)
+                        {
+                            ///calza
+                        }
+                    }
+                    break;
+
+                case Niveau.Moyen:
+                    {
+                        if (nb > nbDeTable)
+                        {
+                            if (de != 0)///perudo
+                            {
+                                ///TODO
+                            }
+                            else if (de < 6)
+                            {
+                                ///encherir sur de
+                            }
+                            else
+                            {
+                                Random rng = new Random();
+                                int choix = rng.Next(0, 2);
+                                if (choix == 0)
+                                {
+                                    ///bluff
+                                }
+                                else if (choix == 1)
+                                {
+                                    ///calza
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Random rng = new Random();
+                            int choix = rng.Next(0, 2);
+                            if (choix == 0)
+                            {
+                                ///encherir sur de
+                            }
+                            else if (choix == 1)
+                            {
+                                ///enchrir sur nb
+                            }
+                        }
+                    }
+                    break;
+            }
+
+        }
+
+
+        public override Action Jouer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Resultat(int idJoueur, bool perdu)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Perudo/Perudo/Perudo/Views/Page2.xaml.cs
+++ Perudo/Perudo/Perudo/Views/Page2.xaml.cs
@@ -22,36 +22,46 @@ namespace Perudo.Views
 	    void submit_Clicked(object sender, EventArgs e)
 	    {
 
-	        var Randomizer = new Randomizer();
-            int humains = Int32.Parse(Humans.Text);
-            int machine = Int32.Parse(Machine.Text);
+            var Randomizer = new Randomizer();
+	        int humains = 0;
+	        int machine = 0;
 
-	        Partie mainPartie = null;
-            
+            try
+	        {
+                humains = Int32.Parse(Humans.Text);
+                machine = Int32.Parse(Machine.Text);
 
-            if (CheckMaxJoueurs(humains, machine))
-            {
-                Debug.WriteLine("La partie va commencer");
-                new Partie(humains + machine, Randomizer);
+	            Partie mainPartie = null;
 
-                var np = new NavigationPage(new Page3());
-                Application.Current.MainPage = np;
+
+	            if (CheckMaxJoueurs(humains, machine))
+	            {
+	                Debug.WriteLine("La partie va commencer");
+	                new Partie(humains + machine, Randomizer);
+
+	                var np = new NavigationPage(new Page3());
+	                Application.Current.MainPage = np;
+	            }
+	            else
+	            {
+	                Debug.WriteLine("La partie ne peux pas démarrer elle a trop de joueur");
+	                Alert.Text = "Le nombre de joueurs ne doit pas dépasser 6";
+
+                    Alert.IsVisible = true;
+	            }
             }
-            else
-            {
-                Debug.WriteLine("La partie ne peux pas démarrer elle a trop de joueur");
-                Alert.IsVisible = true;
-            }
-           
+	        catch (Exception exception)
+	        {
+	            Debug.WriteLine(exception);
+	            Alert.Text = "Doit contenir des chiffres";
+	            Alert.IsVisible = true;
+	        }
             //TODO: ctr IA.
           
 	    }
-
-	    
-
 	    public static bool CheckMaxJoueurs(int nbHumains, int nbIA)
 	    {
-	        return nbHumains + nbIA <= 6;
+	        return nbHumains + nbIA <= 6 && nbHumains + nbIA > 0;
 	    }
     }
 }
