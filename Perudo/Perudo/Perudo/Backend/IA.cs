a84cbb954242037447763e84223909a201f77ef9
Elsa <elsa.demaine@epsi.fr>
2017-12-13 11:55:43 +0100

    Modification  méthode Jouer

--- Perudo/Perudo/Perudo/Backend/IA.cs
+++ Perudo/Perudo/Perudo/Backend/IA.cs
@@ -47,199 +47,198 @@ namespace Perudo.Backend
             else
             {
                 Decision olddec = decs[decs.Count - 1];
-            }
 
-            double p1 = 0.5;
-            double p2 = 0.6;
-            int Z = 1;
-            int Y = CalculY(olddec);
-            int X = CalculX(olddec);
-            switch (myNiveau)
-            {
-                case Niveau.Facile:
-                    { 
-                        Random rng = new Random();
-                        int choix = rng.Next(0, 6);
-                        if (choix == 0)
-                        {
-                            dec = new Decision(Backend.Action.bluff);
-                            return dec;
-                        }
-                        else if (choix == 1)
-                        {
-                            if (olddec.de+1 > 6)
+                double p1 = 0.5;
+                double p2 = 0.6;
+                int Z = 1;
+                int Y = CalculY(olddec);
+                int X = CalculX(olddec);
+                switch (myNiveau)
+                {
+                    case Niveau.Facile:
+                        { 
+                            Random rng = new Random();
+                            int choix = rng.Next(0, 6);
+                            if (choix == 0)
                             {
-                                dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
-                                return dec;
-                            } else
-                            {
-                                dec = new Decision(Backend.Action.encherir, olddec.de + 1, olddec.nb);
+                                dec = new Decision(Backend.Action.bluff);
                                 return dec;
                             }
-                        }
-                        else if (choix == 2)
-                        {
-                            dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
-                            return dec;
-                        }
-                        else if (choix == 3)
-                        {
-                            double res = olddec.nb / 2;
-                            double rnd = Math.Round(res);
-                            int paco = Convert.ToInt32(rnd);
-                            dec = new Decision(Backend.Action.encherir, 1, paco);
-                            return dec;
-                        }
-                        else if (choix == 4)
-                        {
-                            if (olddec.de + 1 > 6)
+                            else if (choix == 1)
+                            {
+                                if (olddec.de+1 > 6)
+                                {
+                                    dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
+                                    return dec;
+                                } else
+                                {
+                                    dec = new Decision(Backend.Action.encherir, olddec.de + 1, olddec.nb);
+                                    return dec;
+                                }
+                            }
+                            else if (choix == 2)
                             {
                                 dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
                                 return dec;
                             }
-                            else
-                            {
-                                dec = new Decision(Backend.Action.encherir, olddec.de + 1, olddec.nb + 1);
-                                return dec;
-                            }
-                        }
-                        else if (choix == 5)
-                        {
-                            dec = new Decision(Backend.Action.calza);
-                            return dec;
-                        }
-                    }
-                    break;
-                   
-
-                case Niveau.Moyen:
-                    {
-                        double T = X * p1 + Y * p2;
-                        if (T < Z - 0.10)
-                        {
-                            dec = new Decision(Backend.Action.bluff);
-                        }
-                        else if (T > Z + 0.10)
-                        {
-                            if (Y >= 0 && Y < 5 )
-                            {
-                                dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb+1);
-
-                            }
-                            else if (olddec.de+1 <= 6 )
-                            {
-                                dec = new Decision(Backend.Action.encherir, olddec.de+1, olddec.nb);
-                            }
-                            else
+                            else if (choix == 3)
                             {
                                 double res = olddec.nb / 2;
                                 double rnd = Math.Round(res);
                                 int paco = Convert.ToInt32(rnd);
                                 dec = new Decision(Backend.Action.encherir, 1, paco);
+                                return dec;
                             }
-                        }
-                        else
-                        {
-                            dec = new Decision(Backend.Action.calza);
-                        }
-                    }
-                    return dec;
-
-                case Niveau.Difficile:
-                    {
-                        int paco = 0;
-                        int deux = 0;
-                        int trois = 0;
-                        int quatre = 0;
-                        int cinq = 0;
-                        int six = 0;
-                        Random rng = new Random();
-                        int choix = rng.Next(1, 11);
-                        
-                        for (int i = 0; i < listDes.Count - 1; i++)
-                        {
-                            if (listDes[i].valeur == "1")
-                            {
-                                paco++;
-                            }
-                            else if (listDes[i].valeur == "2")
-                            {
-                                deux++;
-                            }
-                            else if (listDes[i] .valeur == "3")
-                            {
-                                trois++;
-                            }
-                            else if (listDes[i].valeur == "4")
-                            {
-                                quatre++;
-                            }
-                            else if (listDes[i].valeur == "5")
-                            {
-                                cinq++;
-                            }
-                            else if (listDes[i].valeur == "6")
-                            {
-                                six++;
-                            }
-
-                        }
-
-                        if ((olddec.de == 1 && olddec.nb == paco) ||
-                            (olddec.de == 2 && olddec.nb == deux) ||
-                            (olddec.de == 3 && olddec.nb == trois) ||
-                            (olddec.de == 4 && olddec.nb == quatre) ||
-                            (olddec.de == 5 && olddec.nb == cinq) ||
-                            (olddec.de == 6 && olddec.nb == six) )
-                        {
-                            dec = new Decision(Backend.Action.calza);
-                        }
-                        else if ((olddec.de == 1 && olddec.nb > paco) ||
-                            (olddec.de == 2 && olddec.nb > deux) ||
-                            (olddec.de == 3 && olddec.nb > trois) ||
-                            (olddec.de == 4 && olddec.nb > quatre) ||
-                            (olddec.de == 5 && olddec.nb > cinq) ||
-                            (olddec.de == 6 && olddec.nb > six))
-                        {
-                            dec = new Decision(Backend.Action.bluff);
-                        }
-                        else
-                        {
-                            dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
-                        }
-
-                        if (choix > 6)
-                        {
-                            if (choix == 7)
-                            {
-                                dec = new Decision(Backend.Action.bluff);
-                            }
-                            else if (choix == 8)
+                            else if (choix == 4)
                             {
                                 if (olddec.de + 1 > 6)
                                 {
                                     dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
+                                    return dec;
                                 }
                                 else
                                 {
-                                    dec = new Decision(Backend.Action.encherir, olddec.de + 1, olddec.nb);
+                                    dec = new Decision(Backend.Action.encherir, olddec.de + 1, olddec.nb + 1);
+                                    return dec;
                                 }
                             }
-                            else if (choix == 9)
+                            else if (choix == 5)
+                            {
+                                dec = new Decision(Backend.Action.calza);
+                                return dec;
+                            }
+                        }
+                        break;
+                   
+
+                    case Niveau.Moyen:
+                        {
+                            double T = X * p1 + Y * p2;
+                            if (T < Z - 0.10)
+                            {
+                                dec = new Decision(Backend.Action.bluff);
+                            }
+                            else if (T > Z + 0.10)
+                            {
+                                if (Y >= 0 && Y < 5 )
+                                {
+                                    dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb+1);
+
+                                }
+                                else if (olddec.de+1 <= 6 )
+                                {
+                                    dec = new Decision(Backend.Action.encherir, olddec.de+1, olddec.nb);
+                                }
+                                else
+                                {
+                                    double res = olddec.nb / 2;
+                                    double rnd = Math.Round(res);
+                                    int paco = Convert.ToInt32(rnd);
+                                    dec = new Decision(Backend.Action.encherir, 1, paco);
+                                }
+                            }
+                            else
+                            {
+                                dec = new Decision(Backend.Action.calza);
+                            }
+                        }
+                        return dec;
+
+                    case Niveau.Difficile:
+                        {
+                            int paco = 0;
+                            int deux = 0;
+                            int trois = 0;
+                            int quatre = 0;
+                            int cinq = 0;
+                            int six = 0;
+                            Random rng = new Random();
+                            int choix = rng.Next(1, 11);
+                        
+                            for (int i = 0; i < listDes.Count - 1; i++)
+                            {
+                                if (listDes[i].valeur == "1")
+                                {
+                                    paco++;
+                                }
+                                else if (listDes[i].valeur == "2")
+                                {
+                                    deux++;
+                                }
+                                else if (listDes[i] .valeur == "3")
+                                {
+                                    trois++;
+                                }
+                                else if (listDes[i].valeur == "4")
+                                {
+                                    quatre++;
+                                }
+                                else if (listDes[i].valeur == "5")
+                                {
+                                    cinq++;
+                                }
+                                else if (listDes[i].valeur == "6")
+                                {
+                                    six++;
+                                }
+
+                            }
+
+                            if ((olddec.de == 1 && olddec.nb == paco) ||
+                                (olddec.de == 2 && olddec.nb == deux) ||
+                                (olddec.de == 3 && olddec.nb == trois) ||
+                                (olddec.de == 4 && olddec.nb == quatre) ||
+                                (olddec.de == 5 && olddec.nb == cinq) ||
+                                (olddec.de == 6 && olddec.nb == six) )
+                            {
+                                dec = new Decision(Backend.Action.calza);
+                            }
+                            else if ((olddec.de == 1 && olddec.nb > paco) ||
+                                (olddec.de == 2 && olddec.nb > deux) ||
+                                (olddec.de == 3 && olddec.nb > trois) ||
+                                (olddec.de == 4 && olddec.nb > quatre) ||
+                                (olddec.de == 5 && olddec.nb > cinq) ||
+                                (olddec.de == 6 && olddec.nb > six))
+                            {
+                                dec = new Decision(Backend.Action.bluff);
+                            }
+                            else
                             {
                                 dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
                             }
-                            else if (choix == 10)
+
+                            if (choix > 6)
                             {
-                                dec = new Decision(Backend.Action.calza);
-                            }  
-                        } 
+                                if (choix == 7)
+                                {
+                                    dec = new Decision(Backend.Action.bluff);
+                                }
+                                else if (choix == 8)
+                                {
+                                    if (olddec.de + 1 > 6)
+                                    {
+                                        dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
+                                    }
+                                    else
+                                    {
+                                        dec = new Decision(Backend.Action.encherir, olddec.de + 1, olddec.nb);
+                                    }
+                                }
+                                else if (choix == 9)
+                                {
+                                    dec = new Decision(Backend.Action.encherir, olddec.de, olddec.nb + 1);
+                                }
+                                else if (choix == 10)
+                                {
+                                    dec = new Decision(Backend.Action.calza);
+                                }  
+                            } 
 
-                    }
-                    return dec;
+                        }
+                        return dec;
+                }
+                return null;
             }
-            return null;
-
         }
 
         int CalculX(Decision olddec)
